e6b2018e7ccdb4ff739c1a5857f11ec1baed3ba8
manas16may <manas16may@gmail.com>
2024-07-11 01:40:27 +0530

    adding app file

--- app.py
+++ app.py
@@ -2,34 +2,55 @@ import json
 import pickle
 
 from flask import Flask,request,app,jsonify,url_for,render_template
+from geopy.distance import geodesic
 import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
 ## Load the model
 regmodel=pickle.load(open('final_model.pkl','rb'))
+
+def feature_engineering(data):
+    data.replace('', float(np.nan), regex=True,inplace=True)
+    data.replace(' ', float(np.nan), regex=True,inplace=True)
+    data['Order_Date']=pd.to_datetime(data['Order_Date'],format="%d-%m-%Y")
+    data["month"] = data.Order_Date.dt.month.astype(int)
+    data["quarter"] = data.Order_Date.dt.quarter.astype(int)
+    data['day_of_week'] = data.Order_Date.dt.day_of_week.astype(int)
+    data["is_month_start"] = data.Order_Date.dt.is_month_start.astype(int)
+    data["is_month_end"] = data.Order_Date.dt.is_month_end.astype(int)
+    data["is_quarter_start"] = data.Order_Date.dt.is_quarter_start.astype(int)
+    data["is_quarter_end"] = data.Order_Date.dt.is_quarter_end.astype(int)
+    data["is_year_start"] = data.Order_Date.dt.is_year_start.astype(int)
+    data["is_year_end"] = data.Order_Date.dt.is_year_end.astype(int)
+    data.drop([ 'Order_Date'], axis=1, inplace=True)
+    data['distance']=np.zeros(len(data.index))
+    restaurant_coordinates=data[['Restaurant_latitude','Restaurant_longitude']].to_numpy()
+    delivery_location_coordinates=data[['Delivery_location_latitude','Delivery_location_longitude']].to_numpy()
+    data['distance'] = np.array([geodesic(restaurant, delivery) for restaurant, delivery in zip(restaurant_coordinates, delivery_location_coordinates)])
+    data['distance']= data['distance'].str.extract(r'(\d+\.\d+)').astype(float)
+    for x in data.columns:
+        data[x]=data[x].astype("float64")
+    return data
+
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
-
 @app.route('/predict',methods=['POST'])
 def predict():
-    data=[float(x) for x in request.form.values()]
-    print(request.form)
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    labels=[x for x in request.form.keys()]
+    values=[x for x in request.form.values()]
+    dictt={}
+    for x in request.form.keys():
+        dictt[x]=request.form[x]
+    data=pd.DataFrame(dictt,index=[0])
+    cols_when_model_builds = regmodel.get_booster().feature_names
+    final_input=feature_engineering(data)
+    output=regmodel.predict(final_input[cols_when_model_builds])[0]
+    return render_template("home.html",prediction_text="Time taken is {} minutes".format(output))
 
 
 
